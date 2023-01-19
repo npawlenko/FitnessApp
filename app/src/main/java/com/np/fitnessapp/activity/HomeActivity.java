335b6cce188086f090d6cc9c589dabb1e5eed288
Norbert Pawlenko <blinkz8@protonmail.com>
2023-01-20 00:15:35 +0100

    Add pie chart

--- app/src/main/java/com/np/fitnessapp/activity/HomeActivity.java
+++ app/src/main/java/com/np/fitnessapp/activity/HomeActivity.java
@@ -1,16 +1,73 @@
 package com.np.fitnessapp.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.np.fitnessapp.FitnessApp;
 import com.np.fitnessapp.R;
 
+import java.util.ArrayList;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private PieChart pieChart;
+    private TextView welcomeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        FitnessApp app = FitnessApp.getInstance();
+
+        welcomeTextView = findViewById(R.id.welcome_textview);
+        welcomeTextView.setText(getString(R.string.welcome, app.getUser().name));
+
+        pieChart = findViewById(R.id.home_piechart);
+        setupPieChart();
+        loadPieChartData();
+    }
+
+    private void setupPieChart() {
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setUsePercentValues(true);
+        pieChart.setEntryLabelTextSize(12);
+        pieChart.setEntryLabelColor(Color.BLACK);
+        pieChart.getDescription().setEnabled(false);
+
+        Legend l = pieChart.getLegend();
+        l.setEnabled(false);
+    }
+
+    private void loadPieChartData() {
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(0.2f, "Spalone kalorie"));
+        entries.add(new PieEntry(0.6f, "Spożyte kalorie"));
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        for (int color: ColorTemplate.MATERIAL_COLORS) {
+            colors.add(color);
+        }
+
+        PieDataSet dataSet = new PieDataSet(entries, "");
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(dataSet);
+        data.setDrawValues(true);
+        data.setValueFormatter(new PercentFormatter(pieChart));
+        data.setValueTextSize(12f);
+        data.setValueTextColor(Color.BLACK);
+
+        pieChart.setData(data);
+        pieChart.invalidate();
     }
 }
