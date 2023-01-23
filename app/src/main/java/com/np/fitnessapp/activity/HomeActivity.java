02791a2e4fc04619c66738fd02dff565c31918a1
Norbert Pawlenko <blinkz8@protonmail.com>
2023-01-23 12:06:32 +0100

    Add bottom navigation item selected listener

--- app/src/main/java/com/np/fitnessapp/activity/HomeActivity.java
+++ app/src/main/java/com/np/fitnessapp/activity/HomeActivity.java
@@ -1,10 +1,12 @@
 package com.np.fitnessapp.activity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
@@ -13,13 +15,17 @@ import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.np.fitnessapp.FitnessApp;
 import com.np.fitnessapp.R;
+import com.np.fitnessapp.activity.user.UserSelectActivity;
 
 import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private RecyclerView mealsRecyclerView;
+    private RecyclerView exercisesRecyclerView;
     private PieChart pieChart;
     private TextView welcomeTextView;
 
@@ -29,6 +35,22 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         FitnessApp app = FitnessApp.getInstance();
 
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            switch(id){
+                case R.id.page_2:
+                    Intent diaryIntent = new Intent(this, DiaryActivity.class);
+                    startActivity(diaryIntent);
+                    break;
+                case R.id.page_3:
+                    Intent userSelectIntent = new Intent(this, UserSelectActivity.class);
+                    startActivity(userSelectIntent);
+                default:
+            }
+            return true;
+        });
+
         welcomeTextView = findViewById(R.id.welcome_textview);
         welcomeTextView.setText(getString(R.string.welcome, app.getUser().name));
 
@@ -36,7 +58,8 @@ public class HomeActivity extends AppCompatActivity {
         setupPieChart();
         loadPieChartData();
 
-
+        mealsRecyclerView = findViewById(R.id.meals_recyclerview);
+        exercisesRecyclerView = findViewById(R.id.exercises_recyclerview);
     }
 
     private void setupPieChart() {
